82261f5705743e51826ddad8e17bc96dcecc133d
sharmavins23 <sharmavins23@gmail.com>
2022-05-03 19:22:13 -0400

    added example input files

--- encodeLSBStego.py
+++ encodeLSBStego.py
@@ -1,7 +1,19 @@
 
+
 # ===== Driver code ============================================================
 
 def main():
+    # Read the input visible file to a dictionary object of RGB tuples
+    visibleFilename = ""
+
+    # Read the input hidden file to a dictionary object
+
+    # Verify sizing parameters of hidden and input files
+
+    # Encode the hidden file bits into the visible file bits
+
+    # Reform this into an image and write to output directory
+
     pass
 
 
